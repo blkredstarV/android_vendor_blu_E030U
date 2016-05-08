10b78520830ed931acfe5121b4811146354c136d
Vampirefo <vampirefo@yahoo.com>
2016-05-08 10:18:36 -0400

    first commit

---
+++ E030U-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/E030U/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/E030U/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/E030U/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/E030U/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/E030U/xbin,system/xbin)
+
